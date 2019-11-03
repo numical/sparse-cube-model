12b6a401aeefd2cd2c59b90673fa47fe057c4de4
Mike Evans <mike.evans@numical.com>
2019-11-03 21:52:55 +0000

    add metadata to Sparse3DArray

--- src/Sparse3DArray.js
+++ src/Sparse3DArray.js
@@ -1,38 +1,79 @@
-const getOrCreateArrayAtIndex = (parentArray, index) => {
-  if (!parentArray[index]) {
-    parentArray[index] = [];
+const maxIndexOfObject = (object, depth = 0) => {
+  if (depth === 0) {
+    const indices = Object.keys(object)
+      .map(k => Number.parseInt(k, 10))
+      .filter(n => !Number.isNaN(n))
+      .sort((a, b) => b - a);
+    return indices.length === 0 ? -1 : indices[0];
+  } else {
+    return Object.values(object).reduce((maxIndex, value) => {
+      if (typeof value === "object") {
+        const objMaxIndex = maxIndexOfObject(value, depth - 1);
+        return objMaxIndex > maxIndex ? objMaxIndex : maxIndex;
+      } else {
+        return maxIndex;
+      }
+    }, -1);
   }
-  return parentArray[index];
 };
 
 class Sparse3DArray {
-  #data;
-
+  meta;
   constructor() {
-    this.#data = [];
+    this.meta = { objects: 0, values: 0, lengths: { x: 0, y: 0, z: 0 } };
     ["get", "set", "unset"].forEach(
       method => (this[method] = this[method].bind(this))
     );
   }
 
   get(x, y, z) {
-    const d = this.#data;
-    return d[x] && d[x][y] && d[x][y][z] ? d[x][y][z][0] : undefined;
+    return this[x] && this[x][y] ? this[x][y][z] : undefined;
   }
 
   set(x, y, z, value) {
-    const d = this.#data;
-    const dx = getOrCreateArrayAtIndex(d, x);
-    const dy = getOrCreateArrayAtIndex(dx, y);
-    const dz = getOrCreateArrayAtIndex(dy, z);
-    dz[0] = value;
+    if (!this[x]) {
+      this[x] = {};
+      this.meta.objects++;
+      if (x >= this.meta.lengths.x) {
+        this.meta.lengths.x = x + 1;
+      }
+    }
+    if (!this[x][y]) {
+      this[x][y] = {};
+      this.meta.objects++;
+      if (y >= this.meta.lengths.y) {
+        this.meta.lengths.y = y + 1;
+      }
+    }
+    this[x][y][z] = value;
+    this.meta.values++;
+    if (z >= this.meta.lengths.z) {
+      this.meta.lengths.z = z + 1;
+    }
     return value;
   }
 
   unset(x, y, z) {
-    if (this.get(x, y, z) != null) {
-      const d = this.#data;
-      d[x][y][z] = [];
+    if (this[x] && this[x][y]) {
+      delete this[x][y][z];
+      this.meta.values--;
+      if (z + 1 === this.meta.lengths.z) {
+        this.meta.lengths.z = maxIndexOfObject(this, 2) + 1;
+      }
+      if (Object.keys(this[x][y]).length === 0) {
+        delete this[x][y];
+        this.meta.objects--;
+        if (y + 1 === this.meta.lengths.y) {
+          this.meta.lengths.y = maxIndexOfObject(this, 1) + 1;
+        }
+        if (Object.keys(this[x]).length === 0) {
+          delete this[x];
+          this.meta.objects--;
+          if (x + 1 === this.meta.lengths.x) {
+            this.meta.lengths.x = maxIndexOfObject(this) + 1;
+          }
+        }
+      }
     }
   }
 }
--- src/Sparse3DArray.test.js
+++ src/Sparse3DArray.test.js
@@ -2,43 +2,67 @@ const { test } = require("tap");
 const Sparse3DArray = require("./Sparse3DArray");
 
 test("Array creation", t => {
-  const { get, set, unset } = new Sparse3DArray();
+  const { get, set, unset, meta } = new Sparse3DArray();
   t.type(get, "function");
   t.type(set, "function");
   t.type(unset, "function");
+  t.type(meta, "object");
   t.end();
 });
 
-test("Empty array", t => {
-  const { get } = new Sparse3DArray();
+test("Empty", t => {
+  const { get, meta } = new Sparse3DArray();
   t.type(get(), "undefined");
   t.type(get(0), "undefined");
   t.type(get(0, 0), "undefined");
   t.type(get(0, 0, 0), "undefined");
   t.type(get(10, 10, 10), "undefined");
+  t.same(meta, { objects: 0, values: 0, lengths: { x: 0, y: 0, z: 0 } });
   t.end();
 });
 
 test("Single value at (0,0,0)", t => {
-  const { get, set } = new Sparse3DArray();
+  const s3a = new Sparse3DArray();
+  const { get, set, meta } = s3a;
   const value = {};
   t.equal(set(0, 0, 0, value), value);
   t.equal(get(0, 0, 0), value);
+  t.equal(s3a[0][0][0], value);
   t.type(get(0), "undefined");
   t.type(get(0, 1), "undefined");
   t.type(get(0, 0, 1), "undefined");
+  t.same(meta, { objects: 2, values: 1, lengths: { x: 1, y: 1, z: 1 } });
   t.end();
 });
 
 test("Single value at (10,10,10)", t => {
-  const { get, set } = new Sparse3DArray();
+  const s3a = new Sparse3DArray();
+  const { get, set, meta } = s3a;
   const value = { foo: "bar" };
   t.equal(set(10, 10, 10, value), value);
   t.equal(get(10, 10, 10), value);
+  t.equal(s3a[10][10][10], value);
   t.type(get(10, 10, 0), "undefined");
   t.type(get(10, 0, 10), "undefined");
   t.type(get(10, 0, 0), "undefined");
   t.type(get(0, 0, 0), "undefined");
+  t.same(meta, { objects: 2, values: 1, lengths: { x: 11, y: 11, z: 11 } });
+  t.end();
+});
+
+test("Multiple value at different places", t => {
+  const s3a = new Sparse3DArray();
+  const { get, set, meta } = s3a;
+  t.equal(set(3, 5, 7, 1), 1);
+  t.equal(set(8, 6, 4, 2), 2);
+  t.equal(set(8, 6, 5, 3), 3);
+  t.equal(get(3, 5, 7), 1);
+  t.equal(s3a[3][5][7], 1);
+  t.equal(get(8, 6, 4), 2);
+  t.equal(s3a[8][6][4], 2);
+  t.equal(get(8, 6, 5), 3);
+  t.equal(s3a[8][6][5], 3);
+  t.same(meta, { objects: 4, values: 3, lengths: { x: 9, y: 7, z: 8 } });
   t.end();
 });
 
@@ -54,18 +78,48 @@ test("Reset single value)", t => {
 });
 
 test("Unset undefined does nothing", t => {
-  const { get, set, unset } = new Sparse3DArray();
+  const { get, unset } = new Sparse3DArray();
   unset(5, 5, 0);
   t.type(get(5, 5, 0), "undefined");
   t.end();
 });
 
-test("Unset", t => {
-  const { get, set, unset } = new Sparse3DArray();
+test("Unset single value", t => {
+  const { get, set, unset, meta } = new Sparse3DArray();
   const value = {};
   set(5, 5, 0, value);
   t.equal(get(5, 5, 0), value);
+  t.same(meta, { objects: 2, values: 1, lengths: { x: 6, y: 6, z: 1 } });
   unset(5, 5, 0);
   t.type(get(5, 5, 0), "undefined");
+  t.same(meta, { objects: 0, values: 0, lengths: { x: 0, y: 0, z: 0 } });
+  t.end();
+});
+
+test("Unset from multiple values - metadata", t => {
+  const { get, set, unset, meta } = new Sparse3DArray();
+  t.equal(set(1, 0, 0, 1), 1);
+  t.equal(set(0, 2, 0, 2), 2);
+  t.equal(set(0, 0, 3, 3), 3);
+  t.same(meta, { objects: 5, values: 3, lengths: { x: 2, y: 3, z: 4 } });
+  unset(1, 0, 0);
+  t.same(meta, { objects: 3, values: 2, lengths: { x: 1, y: 3, z: 4 } });
+  unset(0, 2, 0);
+  t.same(meta, { objects: 2, values: 1, lengths: { x: 1, y: 1, z: 4 } });
+  unset(0, 0, 3);
+  t.same(meta, { objects: 0, values: 0, lengths: { x: 0, y: 0, z: 0 } });
+  t.end();
+});
+
+test("Unset from multiple overlapping values - metadata", t => {
+  const { get, set, unset, meta } = new Sparse3DArray();
+  t.equal(set(3, 5, 7, 1), 1);
+  t.equal(set(7, 5, 4, 2), 2);
+  t.equal(set(8, 6, 5, 3), 3);
+  t.same(meta, { objects: 6, values: 3, lengths: { x: 9, y: 7, z: 8 } });
+  unset(3, 5, 7);
+  t.same(meta, { objects: 4, values: 2, lengths: { x: 9, y: 7, z: 6 } });
+  unset(8, 6, 5);
+  t.same(meta, { objects: 2, values: 1, lengths: { x: 8, y: 6, z: 5 } });
   t.end();
 });
